934c29c769e79134409115df6ac76ce3389428c1
AaronDLP <aaron@doublelinepartners.com>
2014-12-15 09:24:18 -0600

    additional tests for sample distributions

--- edfi.sdg.test/utility/RandomExtensions.cs
+++ edfi.sdg.test/utility/RandomExtensions.cs
@@ -3,10 +3,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace edfi.sdg.test.utility
 {
-    using System.Collections.Generic;
-    using System.Threading;
-    using System.Threading.Tasks;
-
     using edfi.sdg.utility;
 
     [TestClass]
@@ -17,25 +13,60 @@ namespace edfi.sdg.test.utility
         {
             var r = new Random();
             var values = new int[41];
-            const int sample = 1 << 20;
+            const int Sample = 1 << 20;
 
-            const double mu = 0.0;
-            const double sigma = 1.0;
+            const double Mu = 0.0;
+            const double Sigma = 1.0;
 
-            for (int i = 0; i < sample; i++)
+            for (var i = 0; i < Sample; i++)
             {
-                var val = r.NextNormal(0, 1.0);
+                var val = r.NextNormal(Mu, Sigma);
                 var idx = (int)Math.Round(val * 10) + 21;
-                if (idx >= values.GetLowerBound(0) && idx <= values.GetUpperBound(0))
-                    Interlocked.Increment(ref values[idx]);
+                if (idx >= values.GetLowerBound(0) && idx <= values.GetUpperBound(0)) values[idx]++;
             }
 
             foreach (var value in values)
             {
-                Console.WriteLine(new string('*', value / (sample >> 10)));
+                Console.WriteLine(new string('*', value / (Sample >> 10)));
             }
         }
 
+        [TestMethod]
+        public void NextChi()
+        {
+            var r = new Random();
+            var values = new int[20];
+            const double Sigma = 1.0;
+            const int Sample = 1 << 20;
+            for (var i = 0; i < Sample; i++)
+            {
+                var val = r.NextChi(1, Sigma);
+                var idx = (int)Math.Round(val * 10);
+                if (idx >= values.GetLowerBound(0) && idx <= values.GetUpperBound(0)) values[idx]++;
+            }
+            foreach (var value in values)
+            {
+                Console.WriteLine(new string('*', value / (Sample >> 9)));
+            }
+        }
 
+        [TestMethod]
+        public void NextChiSquare()
+        {
+            var r = new Random();
+            var values = new int[20];
+            const double Sigma = 1.0;
+            const int Sample = 1 << 20;
+            for (var i = 0; i < Sample; i++)
+            {
+                var val = r.NextChiSquare(1, Sigma);
+                var idx = (int)Math.Round(val * 10);
+                if (idx >= values.GetLowerBound(0) && idx <= values.GetUpperBound(0)) values[idx]++;
+            }
+            foreach (var value in values)
+            {
+                Console.WriteLine(new string('*', value / (Sample >> 8)));
+            }
+        }
     }
 }
