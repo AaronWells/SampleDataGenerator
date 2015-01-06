8bc701cfbaa6b2a153dad7841489ca3c3e9147f7
dan.daneshgar <dan.daneshgar@doublelinepartners.com>
2015-01-06 15:02:12 -0600

    -merged unit test projects to one -added newly made data files

---
+++ edfi.sdg.test/database/UnitTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using edfi.sdg.entity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace edfi.sdg.test.database
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void next_random_family_name_should_not_be_null()
+        {
+            var value = new DataAccess().GetNextValue("FamilyName", new []{"OldEthnicityType.AsianOrPacificIslander", "OldEthnicityType.Hispanic"});
+
+            Console.WriteLine(value);
+            Assert.AreNotEqual(null, value);
+        }
+    }
+}
