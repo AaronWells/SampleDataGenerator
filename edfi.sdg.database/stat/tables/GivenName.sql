70aad7dec182594bd769f07707a77885b349c66f
dan.daneshgar <dan.daneshgar@doublelinepartners.com>
2015-01-06 15:03:54 -0600

    -fixed table structure

--- edfi.sdg.database/stat/tables/GivenName.sql
+++ edfi.sdg.database/stat/tables/GivenName.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [stat].[GivenName] (
     [Id]        BIGINT         IDENTITY (1, 1) NOT NULL,
     [Value]     NVARCHAR (MAX) NOT NULL,
-    [Attribute] NVARCHAR (MAX) NOT NULL,
+    [Attribute] NVARCHAR (400) NOT NULL,
     [Prop100k]  DECIMAL (12, 4) NOT NULL
 );
