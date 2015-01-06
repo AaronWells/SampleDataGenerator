0ae4264afd89b82a394d9ef48b8c80d865f77715
dan.daneshgar <dan.daneshgar@doublelinepartners.com>
2015-01-06 09:23:10 -0600

    added table value test

---
+++ edfi.sdg.test/generators/DataTableValueGeneratorTest.cs
@@ -0,0 +1,36 @@
+﻿using edfi.sdg.generators;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace edfi.sdg.test.generators
+{
+    [TestClass]
+    public class when_try_to_get_the_next_stat_value_from_a_well_populated_stat_table
+    {
+        private string Result { get; set; }
+
+        [TestInitialize]
+        public void InitTest()
+        {
+            var generator = new DataTableValueGenerator
+            {
+                DataTableName = "FamilyName",
+                Attributes = new[] { "OldEthnicityType.AsianOrPacificIslander", "OldEthnicityType.Hispanic" }
+            };
+
+            Result = (string) generator.Generate(null, null)[0];
+        }
+
+        [TestMethod]
+        public void result_should_be_null()
+        {
+            Assert.AreNotEqual(null, Result);
+        }
+
+        [TestMethod]
+        public void should_return_a_value()
+        {
+            Console.WriteLine(Result);
+        }
+    }
+}
