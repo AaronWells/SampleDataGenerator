895a08aac49ffdf186bbdd003253e7c0f106830a
dan.daneshgar <dan.daneshgar@doublelinepartners.com>
2015-01-07 16:39:33 -0600

    -fixed the issue with the GetNextValue sp

--- edfi.sdg.database/dbo/Stored Procedures/GetNextValue.sql
+++ edfi.sdg.database/dbo/Stored Procedures/GetNextValue.sql
@@ -1,29 +1,41 @@
-﻿CREATE procedure GetNextValue
+﻿CREATE procedure [dbo].[GetNextValue]
 	@StatTableName varchar(100), -- a table name in the 'stat' schema (excluding stat.) -- e.g.: FamilyName
 	@AttributeFilters as dbo.AttributeList readonly
 as
 begin
 	declare @command as nvarchar(max) =
-	'with RankedResult 
-	as
-	(
+	'with 
+	RankedResult as
+	(	
 		select 
 			t.id, sum(t.Prop100K) over (order by t.id) as Prop100K
-			from 
-			(
-				select 		id, sum(Prop100K) as Prop100K
-				from		stat.' + @StatTableName + ' a inner join @attributeFilters b on a.Attribute = b.Attribute
-				group by	id
-			) t
+		from 
+		(
+			select 		id, sum(Prop100K) as Prop100K
+			from		stat.' + @StatTableName + '
+			where		Attribute in (select * from @AttributeFilters)
+			group by	id
+		) t
 	)
-	select top 1 map.Value 
-	from 
+	,
+	aggregates as
 	(
-		select id, Prop100k
+		select 
+			max(Prop100K) as maxValue,
+			max(Prop100K) * rand() as randValue
 		from RankedResult 
-		where Prop100K < (select max(Prop100K) * rand() from RankedResult )
-	) t join stat.' + @StatTableName + ' as map on map.id = t.id
-	order by t.Prop100K desc'
+	)
+	select top 1 Value
+	from RankedResult join stat.' + @StatTableName + ' as map 
+		on map.id = RankedResult.id
+	where RankedResult.Prop100K >= (select randValue from aggregates)
+	order by RankedResult.Prop100K'
 
 	execute sp_executesql @command, N'@attributeFilters dbo.AttributeList readonly', @attributeFilters = @AttributeFilters;
+
+/*
+	declare @t as dbo.AttributeList
+	insert into @t values('F')
+	exec GetNextValue 'GivenName', @t
+*/
 end
