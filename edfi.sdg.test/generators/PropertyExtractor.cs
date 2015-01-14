393a0c5798c0b8c2fe1b95198de2bbf4d4272a52
AaronDLP <aaron@doublelinepartners.com>
2015-01-14 08:29:19 -0600

    Property Extractor class and tests

---
+++ edfi.sdg.test/generators/PropertyExtractor.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EdFi.SampleDataGenerator.Test.Generators
+{
+    using System.Linq;
+
+    [TestClass]
+    public class PropertyExtractor
+    {
+        // ReSharper disable once ClassNeverInstantiated.Local
+        private class ClassA
+        {
+            public string StringProperty { get; set; }
+            public int IntProperty { get; set; }
+        }
+
+        private class ClassB
+        {
+            public ClassA ClassAProperty { get; set; }
+            public string[] StringArrayProperty { get; set; }
+        }
+
+        private class ClassC : ClassB
+        {
+            public double DoubleProperty { get; set; }
+        }
+
+        [TestMethod]
+        public void RetrievesAllPropertyPaths()
+        {
+            var result = EdFi.SampleDataGenerator.Generators.PropertyExtractor.ExtractPropertyMetadata(typeof(ClassC));
+            var propertyPaths = result.Select(x => x.PropertyPath).ToArray();
+            Assert.IsTrue(propertyPaths.Contains("ClassAProperty"));
+            Assert.IsTrue(propertyPaths.Contains("StringArrayProperty"));
+            Assert.IsTrue(propertyPaths.Contains("ClassAProperty.StringProperty"));
+            Assert.IsTrue(propertyPaths.Contains("ClassAProperty.IntProperty"));
+            Assert.IsTrue(propertyPaths.Contains("DoubleProperty"));
+        }
+
+        [TestMethod]
+        public void RetrievesAllPropertyNames()
+        {
+            var result = EdFi.SampleDataGenerator.Generators.PropertyExtractor.ExtractPropertyMetadata(typeof(ClassC));
+            var propertyNames = result.Select(x => x.PropertyInfo.Name).ToArray();
+            Assert.IsTrue(propertyNames.Contains("ClassAProperty"));
+            Assert.IsTrue(propertyNames.Contains("StringArrayProperty"));
+            Assert.IsTrue(propertyNames.Contains("StringProperty"));
+            Assert.IsTrue(propertyNames.Contains("IntProperty"));
+            Assert.IsTrue(propertyNames.Contains("DoubleProperty"));
+        }
+
+        [TestMethod]
+        public void RetrievesAllPropertyTypes()
+        {
+            var result = EdFi.SampleDataGenerator.Generators.PropertyExtractor.ExtractPropertyMetadata(typeof(ClassC));
+            var propertyTypes = result.Select(x => x.PropertyInfo.PropertyType).ToArray();
+            Assert.IsTrue(propertyTypes.Contains(typeof(ClassA)));
+            Assert.IsTrue(propertyTypes.Contains(typeof(string[])));
+            Assert.IsTrue(propertyTypes.Contains(typeof(string)));
+            Assert.IsTrue(propertyTypes.Contains(typeof(int)));
+            Assert.IsTrue(propertyTypes.Contains(typeof(double)));
+        }
+    }
+}
---
+++ edfi.sdg/Generators/PropertyExtractor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EdFi.SampleDataGenerator.Generators
+{
+    using System.Reflection;
+
+    public class PropertyMetadata : IComparable<PropertyMetadata>
+    {
+        public string PropertyPath { get; private set; }
+
+        public PropertyInfo PropertyInfo { get; private set; }
+
+        public PropertyMetadata(string propertyPath, PropertyInfo propertyInfo)
+        {
+            PropertyPath = propertyPath;
+            PropertyInfo = propertyInfo;
+        }
+
+        public int CompareTo(PropertyMetadata other)
+        {
+            return System.String.Compare(PropertyPath, other.PropertyPath, System.StringComparison.Ordinal);
+        }
+    }
+
+    public static class PropertyExtractor
+    {
+        public static IEnumerable<PropertyMetadata> ExtractPropertyMetadata(Type type)
+        {
+            var result = new List<PropertyMetadata>();
+            RecursiveGetProperties(type, null, result);
+            return result;
+        }
+
+        private static void RecursiveGetProperties(Type type, string parentPath, ICollection<PropertyMetadata> propertyMetadata)
+        {
+            var properties = type.GetProperties().Where(p => p.CanRead && p.CanWrite);
+
+            foreach (var propinfo in properties)
+            {
+                var path = string.IsNullOrEmpty(parentPath) ? propinfo.Name : String.Join(".", new string[] { parentPath, propinfo.Name });
+                propertyMetadata.Add(new PropertyMetadata(path, propinfo));
+                RecursiveGetProperties(propinfo.PropertyType, path, propertyMetadata);
+            }
+        }
+    }
+}
