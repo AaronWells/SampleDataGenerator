eb627e32b8643f4a2dd7abfc5b9f5db233a1fc43
AaronDLP <aaron@doublelinepartners.com>
2015-01-19 13:16:12 -0600

    updated database project layout and population of sample stat tables

---
+++ edfi.sdg.database/dbo/tables/ComplexObject.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[ComplexObject]
+(
+    [Identifier] CHAR(25) NOT NULL PRIMARY KEY,
+    [ClassName] VARCHAR(MAX) NOT NULL,
+    [Xml] XML NOT NULL, 
+)
+
+GO
+
+CREATE PRIMARY XML INDEX [XML_IX_ComplexObject_Column] ON [dbo].[ComplexObject] ([Xml])
+
+GO
