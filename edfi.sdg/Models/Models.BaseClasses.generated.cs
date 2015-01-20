05381ca63036a941146aca0d92e3a5b8d7a9b02e
AaronDLP <aaron@doublelinepartners.com>
2015-01-20 17:11:01 -0600

    extend model classes via codegen

---
+++ edfi.sdg/Models/Models.BaseClasses.generated.cs
@@ -0,0 +1,70 @@
+﻿namespace EdFi.SampleDataGenerator.Models {
+
+	public partial class FeederSchoolAssociation : IComplexObjectType { 
+		public string id { get; set; } 
+	}
+
+	public partial class InterchangeStaffAssociation : IComplexObjectType { 
+		public string id { get; set; } 
+	}
+
+	public partial class StaffEducationOrgAssignmentAssociation : IComplexObjectType { 
+		public string id { get; set; } 
+	}
+
+	public partial class StaffEducationOrgEmploymentAssociation : IComplexObjectType { 
+		public string id { get; set; } 
+	}
+
+	public partial class StaffProgramAssociation : IComplexObjectType { 
+		public string id { get; set; } 
+	}
+
+	public partial class TeacherSchoolAssociation : IComplexObjectType { 
+		public string id { get; set; } 
+	}
+
+	public partial class TeacherSectionAssociation : IComplexObjectType { 
+		public string id { get; set; } 
+	}
+
+	public partial class StaffCohortAssociation : IComplexObjectType { 
+		public string id { get; set; } 
+	}
+
+	public partial class StudentCohortAssociation : IComplexObjectType { 
+		public string id { get; set; } 
+	}
+
+	public partial class StudentDisciplineIncidentAssociation : IComplexObjectType { 
+		public string id { get; set; } 
+	}
+
+	public partial class StudentEducationOrganizationAssociation : IComplexObjectType { 
+		public string id { get; set; } 
+	}
+
+	public partial class StudentSchoolAssociation : IComplexObjectType { 
+		public string id { get; set; } 
+	}
+
+	public partial class StudentSectionAssociation : IComplexObjectType { 
+		public string id { get; set; } 
+	}
+
+	public partial class EducationOrganizationInterventionPrescriptionAssociation : IComplexObjectType { 
+		public string id { get; set; } 
+	}
+
+	public partial class StudentInterventionAssociation : IComplexObjectType { 
+		public string id { get; set; } 
+	}
+
+	public partial class StudentParentAssociation : IComplexObjectType { 
+		public string id { get; set; } 
+	}
+
+	public partial class StudentProgramAssociation : IComplexObjectType { 
+		public string id { get; set; } 
+	}
+}
